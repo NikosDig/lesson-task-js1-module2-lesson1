dbd8200c3b9ae0548fee45ec92fb4a0f96df99d2
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-04-19 18:49:36 +0200

    Added teachers examples

--- js/script.js
+++ js/script.js
@@ -36,6 +36,13 @@ prizes.forEach(function (prize, index) {
   console.log("Prize " + (index + 1) + ": " + prize);
 });
 
+//      TEACERS EXAMPLE
+// const prizes = ["Bag of bananas", "Box of frogs", "Car of clowns"];
+
+// prizes.forEach(function (prize, index) {
+//     console.log(`Prize ${index + 1}: ${prize}`);
+// });
+
 // ### Question 3
 
 // Write code that waits 5 seconds before logging the string: `I waited 5 seconds before executing`. Store the amount of seconds (5 initially) in a variable so it can be easily changed.
@@ -45,6 +52,15 @@ let timer = 5000;
 setTimeout(function () {
   console.log("i waited 5 seconds befor executing");
 }, timer);
+
+//      TEACHERS EXAMPLE
+// const numberOfSeconds = 5;
+
+// function logMessage() {
+//     console.log(`I waited ${numberOfSeconds} seconds before executing`);
+// }
+
+// setTimeout(logMessage, numberOfSeconds * 1000);
 
 // ### Question 4
 
@@ -59,3 +75,23 @@ const countingNumbers = setInterval(function () {
   }
   console.log(count);
 }, 1500);
+
+//          TEACHERS EXAMPLE
+// const interval = 1500;
+// let count = 0;
+// const limit = 4;
+
+// function logNumber() {
+//     // increase count's value by 1
+//     count++;
+
+//     // log count's current value
+//     console.log(count);
+
+//     // check whether to clear the setInterval
+//     if (count === limit) {
+//         clearInterval(intervalId);
+//     }
+// }
+
+// const intervalId = setInterval(logNumber, interval);
